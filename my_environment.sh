b57fd736ce5425e56805eeaadb43cff16512e320
alejandro21quiroga <146993011+alejandro21quiroga@users.noreply.github.com>
2024-06-27 14:38:28 +0200

    Update my_environment.sh

--- my_environment.sh
+++ my_environment.sh
@@ -2,7 +2,7 @@
 # Load LCG
 source /cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos7-gcc8-opt/setup.sh
 
-# Set environment variables for pyhia8 and lhapdf libraries. You will need to point the path to your local MadGraph folder; for example, PYTHIA8DATA=/path_to_MadGraph/MadGraph_3_5_1/MG5_aMC_v3_5_1/HEPTools/pythia8/share/Pythia8/xmldoc/. The same for LHAPDF_DATA_PATH.
+# Set environment variables for pyhia8 and lhapdf libraries. You will need to point the path to your local MadGraph folder; for example, PYTHIA8DATA=/path_to_MadGraph/MadGraph_3_5_1/HEPTools/pythia8/share/Pythia8/xmldoc/. The same for LHAPDF_DATA_PATH.
 
 #export PYTHIA8DATA=/PATH_TO_YOUR_PYTHIA_LIBRARY
 #export LHAPDF_DATA_PATH=/PATH_TO_YOUR_LHAPDF_LIBRARY
